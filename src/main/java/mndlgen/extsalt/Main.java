2ec1491b0a7ac46442ea558d621b51d8c21db41d
Ganesh K <ganeshk.work@gmail.com>
2020-11-14 19:34:16 +0530

    add apache kafka dependency

--- src/main/java/mndlgen/extsalt/Main.java
+++ src/main/java/mndlgen/extsalt/Main.java
@@ -5,7 +5,10 @@ package mndlgen.extsalt;
  */
 public class Main {
     public static void main(String... args) {
-        System.out.println("Mandira, this is you last diwali without me. After this, you going to stay with me forever");
+        System.out.println(
+                "Mandira, this is you last diwali without me. " +
+                        "After this, you going to stay with me forever"
+        );
     }
 }
 
