0c50f198dd6405ef4bf7a52e2ac4e13062649174
Ganesh K <ganeshk.work@gmail.com>
2020-11-14 20:56:54 +0530

    add logger, add sample producer for test and add initial setup

--- src/main/java/mndlgen/extsalt/Main.java
+++ src/main/java/mndlgen/extsalt/Main.java
@@ -1,14 +1,29 @@
 package mndlgen.extsalt;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 /**
  * This is where everything starts.
  */
 public class Main {
     public static void main(String... args) {
-        System.out.println(
-                "Mandira, this is you last diwali without me. " +
-                        "After this, you going to stay with me forever"
-        );
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put("acks", "all");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<String, String>("location", "25 77"));
+        }
+
     }
 }
 
