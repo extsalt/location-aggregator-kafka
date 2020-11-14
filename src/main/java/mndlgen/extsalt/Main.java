969892683261b9d63127bba81fe6061e84101a2c
Ganesh K <ganeshk.work@gmail.com>
2020-11-14 19:19:55 +0530

    initial commit

---
+++ src/main/java/mndlgen/extsalt/Main.java
@@ -0,0 +1,11 @@
+package mndlgen.extsalt;
+
+/**
+ * This is where everything starts.
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Mandira, this is you last diwali without me. After this, you going to stay with me forever");
+    }
+}
+
